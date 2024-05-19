a2cffa00e58537c9d50f78a884d6dd431f8129c2
InvraNet <invranet@InvraNets-MacBook-Air.local>
2024-05-19 19:35:49 +1000

    add: video

--- src/js/keybindKFC.js
+++ src/js/keybindKFC.js
@@ -1,6 +1,20 @@
-document.addEventListener("keypress", function(event) {
-    if (event.keyCode == 13) {
-        audio = new Audio("https://invra.net/src/localfunction/dva_5.mp3")
+let keysPressed = [];
+let keyCount = 0;
+
+document.addEventListener("keydown", function(event) {
+    keysPressed.push(event.key);
+    keyCount++;
+    if (keysPressed.join('').includes("dva")) {
+        let audio = new Audio("https://invra.net/src/localfunction/dva_5.mp3");
         audio.play();
+        keysPressed = [];
+    } else if (keysPressed.join('').includes("pissoff")) {
+        let audio = new Audio("https://invra.net/src/localfunction/what.mp3");
+        audio.play();
+        keysPressed = [];
+    } else if (keyCount == 100) {
+        alert("STOP HITTING BUTTONS!")
+    } else if (keyCount == 150) {
+        window.location.href = "https://invra.net/src/localfunction/jbfm.mp4";
     }
-  });
+});
