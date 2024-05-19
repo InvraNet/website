f73a9829e8a5219f0dc67633c36a179f8b15457d
InvraNet <invranet@InvraNets-MacBook-Air.local>
2024-05-19 18:48:19 +1000

    src/js/keybindKFC.js
    fix: keybinding

--- src/js/keybindKFC.js
+++ src/js/keybindKFC.js
@@ -1,5 +1,5 @@
 document.addEventListener("keypress", function(event) {
-    if (event.keyCode == 68) {
+    if (event.keyCode == 13) {
         audio = new Audio("https://invra.net/src/localfunction/dva_5.mp3")
         audio.play();
     }
--- src/js/uri_pointer.js
+++ src/js/uri_pointer.js
@@ -5,6 +5,10 @@ var links = [
     "https://dominoespizzas.com",
     "https://mathsonline.com.au",
     "https://github.com/apple/swift",
-    "https://adoble.com.au"
+    "https://adoble.com.au",
+    "https://zety.com/blog/mcdonalds-resume-example",
+    "https://samsung.com.au",
+    "https://github.com/torvalds",
+    "https://statusquo.co.uk"
 ]
 document.getElementById("abc").href = links[Math.floor(Math.random()*links.length)];; 
