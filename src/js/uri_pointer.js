d2aba197dac80ef2aaabea43b119483929772c24
invranet <identificationsucks@gmail.com>
2024-05-19 15:27:04 +1000

    add: external js handshake.

---
+++ src/js/uri_pointer.js
@@ -0,0 +1,11 @@
+var links = [
+    "https://kfc.com.au",
+    "https://afp.gov.au",
+    "chrome://quit",
+    "https://dominoespizzas.com",
+    "https://mathsonline.com.au",
+    "https://github.com/apple/swift",
+    "https://adoble.com.au"
+]
+document.getElementById("abc").href = links[Math.floor(Math.random()*links.length)];; 
+console.log("test");
