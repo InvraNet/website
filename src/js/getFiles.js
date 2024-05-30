7bb60c1cf121dcb1772a476d492952d866f64c32
Michael <pacstrap@mx.idalon.org>
2024-05-30 15:49:02 +1000

    fix: loading refusing.

--- src/js/getFiles.js
+++ src/js/getFiles.js
@@ -1,7 +1,7 @@
 let fileData;
 
 function fetchFiles() {
-    return fetch('http://api.cdndwnld.invra.net/pub')
+    return fetch('http://cdndwnld.invra.net/pub/downloadmap.json')
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok');
