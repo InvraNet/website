b3ffbd96c2f5ca358ca12634456dec3cc480fb07
invranet <pacstrap@mx.idalon.org>
2024-05-31 19:33:14 +1000

    add: directory support through JSON multiple arrays

--- src/js/getFiles.js
+++ src/js/getFiles.js
@@ -10,35 +10,71 @@ function fetchFiles() {
         })
         .then(data => {
             fileData = data;
-            displayDownloads();
+            displayDownloads(fileData.children);
         })
         .catch(error => {
             console.error('Error fetching repositories:', error);
         });
 }
 
-function displayDownloads() {
+
+function displayDownloads(children, currentPath = "") {
     const fileContainer = document.getElementById('file-container');
     fileContainer.innerHTML = '';
     const fileList = document.createElement('div');
-    fileData.forEach(file => {
-        const fileElem = document.createElement('div');
-        const fileLocation = `https://cdndwnld.invra.net/${file.location}`;
-        fileElem.classList.add('cursor-pointer', 'include-block', 'bg-gray-800', 'rounded-lg', 'p-4', 'shadow-lg', 'mb-3', 'hover::last:text-underline');
-        fileElem.addEventListener("click", function() {
-            window.location.href = fileLocation;
+    if (currentPath && currentPath !== "/") {
+        const upLevelElem = document.createElement('div');
+        upLevelElem.classList.add('cursor-pointer', 'include-block', 'bg-gray-800', 'rounded-lg', 'p-4', 'shadow-lg', 'mb-3', 'hover::last:text-underline');
+        upLevelElem.addEventListener("click", function() {
+            const parentPath = currentPath.split('/').slice(0, -1).join('/');
+            fetchFiles(parentPath);
         });
-        fileElem.innerHTML = `
+        upLevelElem.innerHTML = `
             <div class="group">
                 <div class="flex justify-between items-center">
-                    <p id="name" class="font-semibold text-lg text-primary-500">${file.name}</p>
-                    <span class="text-gray-500">Size: ${file.size}</span>
+                    <p id="name" class="font-semibold text-lg text-primary-500">parent.dir</p>
+                    <span class="text-gray-500"></span>
                 </div>
             </div>
         `;
-        fileList.appendChild(fileElem);
+        fileList.appendChild(upLevelElem);
+    }
+    
+    children.forEach(item => {
+        const itemElem = document.createElement('div');
+        if (item.type === 'file') {
+            const fileLocation = `https://cdndwnld.invra.net/pub/${currentPath}/${item.name}`;
+            itemElem.classList.add('cursor-pointer', 'include-block', 'bg-gray-800', 'rounded-lg', 'p-4', 'shadow-lg', 'mb-3', 'hover::last:text-underline');
+            itemElem.addEventListener("click", function() {
+                window.location.href = fileLocation;
+            });
+            itemElem.innerHTML = `
+                <div class="group">
+                    <div class="flex justify-between items-center">
+                        <p id="name" class="font-semibold text-lg text-primary-500">${item.name}</p>
+                        <span class="text-gray-500">Size: ${item.size}</span>
+                    </div>
+                </div>
+            `;
+        } else if (item.type === 'directory') {
+            itemElem.classList.add('cursor-pointer', 'include-block', 'bg-gray-800', 'rounded-lg', 'p-4', 'shadow-lg', 'mb-3', 'hover::last:text-underline');
+            itemElem.addEventListener("click", function() {
+                // Pass the updated current path to the recursive call
+                displayDownloads(item.children, currentPath !== "" ? `${currentPath}/${item.name}` : item.name);
+            });
+            itemElem.innerHTML = `
+                <div class="group">
+                    <div class="flex justify-between items-center">
+                        <p id="name" class="font-semibold text-lg text-primary-500">${item.name}</p>
+                        <span class="text-gray-500">[Folder]</span>
+                    </div>
+                </div>
+            `;
+        }
+        fileList.appendChild(itemElem);
     });
+    
     fileContainer.appendChild(fileList);
 }
 
-fetchFiles();
+fetchFiles("");
