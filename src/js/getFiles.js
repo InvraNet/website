c94350928b4ec5f6b2ca33edcc18080c21e3e03a
invranet <pacstrap@mx.idalon.org>
2024-05-31 12:37:14 +1000

    fix: security warning HTTP not HTTPS.

--- src/js/getFiles.js
+++ src/js/getFiles.js
@@ -1,7 +1,7 @@
 let fileData;
 
 function fetchFiles() {
-    return fetch('https://invra.net/api/downloadmap.json')
+    return fetch('https://corsproxy.io/?https%3A%2F%2Fservice.api.cdndwnld.invra.net/pub')
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok');
@@ -24,11 +24,14 @@ function displayDownloads() {
     fileData.forEach(file => {
         const fileElem = document.createElement('div');
         const fileLocation = `https://cdndwnld.invra.net/${file.location}`;
-        fileElem.classList.add('include-block', 'bg-gray-800', 'rounded-lg', 'p-4', 'shadow-lg', 'mb-3');
+        fileElem.classList.add('cursor-pointer', 'include-block', 'bg-gray-800', 'rounded-lg', 'p-4', 'shadow-lg', 'mb-3', 'hover::last:text-underline');
+        fileElem.addEventListener("click", function() {
+            window.location.href = fileLocation;
+        });
         fileElem.innerHTML = `
-            <div>
+            <div class="group">
                 <div class="flex justify-between items-center">
-                    <a href="${fileLocation}" class="font-semibold text-lg text-primary-500 hover:underline" target="_blank">${file.name}</a>
+                    <p id="name" class="font-semibold text-lg text-primary-500">${file.name}</p>
                     <span class="text-gray-500">Size: ${file.size}</span>
                 </div>
             </div>
