5c38118d442e51c1d705fc95447c9df7c1230774
Michael <identificationsucks@gmail.com>
2024-06-15 18:32:50 +1000

    Remove warnings

--- src/js/onload.js
+++ src/js/onload.js
@@ -1,7 +1,3 @@
 document.cookie = 'cookieName=cookieValue; SameSite=Strict; Secure';
 document.cookie = 'cookieName=cookieValue; SameSite=Strict; Secure; Partitioned=True';
-
-document.addEventListener('DOMContentLoaded', function() {
-    window.alert("This website will be down by 2:20PM AEDT to transfer certificates from Cloudflare's Universal SSL to Advanced Certificate Manager. This will take an estimated 1 hour, depending on provisioning time.");
-  });
   
