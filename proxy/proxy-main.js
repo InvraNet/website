b80bf2f32ea9069311df6620c8b209b45aa49147
Michael <identificationsucks@gmail.com>
2024-06-15 18:33:28 +1000

    Change port

--- proxy/proxy-main.js
+++ proxy/proxy-main.js
@@ -4,7 +4,7 @@ const fs = require('fs').promises;
 const path = require('path');
 
 const app = express();
-const port = 80;
+const port = 8059;
 
 const allowedHostname = 'invra.net';
 const getClientIP = (req) => {
