cf3185527ade6f5ca0c72f8c365b89eac6a86fd4
michael <pacstrap@mx.idalon.org>
2024-10-30 23:11:21 +1100

    Disasoviate with cloudflare pages.

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
-import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   distDir: 'build',
 };
-
-if (process.env.NODE_ENV === 'development') {
-  await setupDevPlatform();
-}
 
 export default nextConfig;
